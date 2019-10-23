271f5779fabeaf2e71c4db4bed3b2e1b5a2605e3
Aymane Mzily <aymane.mzily@gmail.com>
2019-10-23 22:36:42 +0100

    remove all

--- resources/views/partials/menu.blade.php
+++
@@ -1,7 +0,0 @@
-
-<div style="display:flex;align-items:center;" class="menu-card main-card content-header bootstrap-row-modifier mb-1">
-    <div class="content-header-left col-md-6 col-12">
-            <i class="{{$_SESSION["icon"]}} la-2x"></i>
-        <h3 class= "content-header-title"> <span>{{$_SESSION["name"]}}</span></h3>
-    </div>
-</div>
