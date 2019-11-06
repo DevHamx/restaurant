073a1e0ec59132a3380488aa1ccad7319361e8c9
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2019-11-06 20:12:59 +0100

    add feature:     -now you can add/modifie/delete parent categories with there children categories

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -17,11 +17,11 @@ class CategorieController extends Controller
     public function index()
     {
         $validator = JsValidator::make($this->validationRules);
-        $categories = Category::where("parent_id",null)->pluck('name', 'id');
+        $categories = Category::whereIsRoot()->pluck('name', 'id');
         return view('pages.category')->with([
             'validator'=>$validator,
             'categories'=>$categories
-        ]);
+            ]);
     }
 
     
@@ -45,9 +45,12 @@ class CategorieController extends Controller
             $this->validate($request,[
                 'name' => ['required', 'string', 'max:191','unique:categories,name,'.$category->id],
             ]);
-            
-            $this->updateOrAddCategory($request,$category);                          
-            return \redirect('/category')->with('success','La Categorie a été modifié avec succès.');
+                
+            if (strcmp($name_category=$this->updateOrAddCategory($request,$category),'0')!=0) {
+                return \redirect('/compagnies')->with('error','la sous categore avec le nom: '.$name_category.' ne peut pas être supprimé car elle est lié au moins à une restaurant.');                
+            }
+            else                      
+            return \redirect('/category')->with('success','La Categorie : '.$request->input('name').' a été modifié avec succès.');
                break;
     
             case 'Supprimer':
@@ -55,16 +58,30 @@ class CategorieController extends Controller
                 'id' => ['required','exists:categories'],
             ]);  
             $category = Category::where('id',$request->input('id'))->first();
+            if ($request->input('sousC')==0) {
+                //parent
+                foreach ($category->descendants as $categoryItem) {
+                    if ($categoryItem->restaurants()->exists()) {
+                        return \redirect('/category')->with('error','La categorie parentale avec le nom: '.$request->input('name').' ne peut pas etre supprimé car sa sous categorie: '.$categoryItem->name.' est lié au moins à une restaurant.');
+                    }
+                }
+            }
+            else{
+                //child
+                if ($category->restaurants()->exists()) {
+                    return \redirect('/category')->with('error','la sous categorie avec le nom: '.$request->input('name').' ne peut pas etre supprimé car elle est lié au moins à une restaurant.');
+                }
+            }
             $category->delete();
             return \redirect('/category')->with('success','La Categorie : '.$request->input('name').' a été supprimer avec succès.');
             break;
 
         }
     }
-    public function getData(Request $request)
+    public function getSousCategoriesData(Request $request)
     {
         if ($request->ajax()) {
-            $categories = Category::orderBy('updated_at','desc')->get();                        
+            $categories = Category::hasParent()->orderBy('updated_at','desc')->get();                        
             return Datatables::of($categories)
             ->addColumn('parent_name',function(Category $categorie){
                 $parentName=$categorie->parent_id!=null?Category::find($categorie->parent_id)->name:null;
@@ -75,17 +92,68 @@ class CategorieController extends Controller
         }
     }
 
+    public function getCategoriesParentaleData(Request $request)
+    {
+        if ($request->ajax()) {
+            $categories = Category::whereIsRoot()->orderBy('updated_at','desc')->get();                        
+            return Datatables::of($categories)
+            ->addColumn('sousCategories',function(Category $categorie){
+                $sousCategoriesSelect = $categorie->descendants->pluck('name')->toArray();;
+                    return implode(",",$sousCategoriesSelect);
+            })
+            
+            ->make(true);
+        }
+    }
+
+    public function getSousCategories($id)
+    {
+        $sousCategoriesSelect=Category::descendantsOf($id)->pluck('name', 'id');
+        return \json_encode($sousCategoriesSelect);
+    }
+
     public function updateOrAddCategory(Request $request, $category)
     {
         $category->name=$request->input('name');
         if ($request->input('sousC')==0) {
             //parent
             $category->saveAsRoot();
+            $sousCategories = $category->descendants;
+            for ($i=0; $i < sizeof($sousCategories); $i++) { 
+                if ($sousCategories[$i]->restaurants->count()!=0) {
+                    $sousCategoryExist=false;
+                    for ($j=0; $j < sizeof($sousCategoriesSelect); $j++) { 
+                        if (strcmp($sousCategories[$i]->name,$sousCategoriesSelect[$j])==0) {
+                            $sousCategoryExist=true;
+                            break;
+                        }
+                    }
+                    if (!$sousCategoryExist) 
+                        return $sousCategories[$i]->name;                    
+                }
+                else
+                    $sousCategories[$i]->delete();
+            }
+            for ($i=0; $i < sizeof($sousCategoriesSelect); $i++) { 
+                $selectedCategory=null;
+                $selectedCategory = Category::where('name',$sousCategoriesSelect[$i])->first();
+                if ($selectedCategory==null){
+                    $selectedCategory = new Category();
+                    $selectedCategory->name=$sousCategoriesSelect[$i];
+                    $category->appendNode($selectedCategory);
+                }
+                else{
+                    //$selectedCategory->name=$sousCategoriesSelect[$i];
+                    $selectedCategory->save();
+                }
+            } 
+
+
         } else {
             //child
             $parent = Category::find($request->input('categories'));
             $parent->appendNode($category);
         }
-                  
+        return '0';         
     }
 }
